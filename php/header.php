072ee052fd3e39b628f7579676ba568df1b61bee
KAYOKG <kayo.kg@hotmail.com>
2023-06-17 16:26:55 -0300

    up

--- php/header.php
+++
@@ -1,8 +0,0 @@
-<header class="pagina">
-  <nav>
-    <a class="logo" href="../index.html"><img id="logoNav" src="../images/icons/iconeLogo.svg" alt="logo da empresa">
-      NOVO TEMPO CAFÉ
-    </a>
-
-  </nav>
-</header>
